0774df30ad26bbc434e68eaa93f32061eb404cb0
Micah Chiang <mchiang@rentreporters.com>
2018-01-01 13:51:55 -0800

    initial commit for es6 starter

---
+++ README.md
@@ -0,0 +1,3 @@
+# es6-Webpack starter
+
+Simple es6 and scss starter using webpack to bundle.
---
+++ src/main.js
@@ -0,0 +1,5 @@
+// This is your entry file. Import your primary app.js file here.
+
+// window.onload = () => {
+//    let app = new App(); Initialize your app on load.
+// }
