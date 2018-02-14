496598c9a8fd759331cf3e48592a8bcd10f02b2e
Micah Chiang <micahkchiang@gmail.com>
2018-02-14 15:45:19 -0800

    updated webpack config file, readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # es6-Webpack starter
 
 Simple es6 and scss starter using webpack to bundle.
+
+**Some Useful Resources:**  
+
+Google's fundamentals guide on optimizing performance with webpack. Many of the suggestions there are implemented in this
+webpack config.
+https://developers.google.com/web/fundamentals/performance/webpack/
+
+To optimize dependencies and reduce overall size on page, check this:
+https://github.com/GoogleChromeLabs/webpack-libs-optimizations
+
+To learn more about image compression, and how to use customize image-webpack-loader:
+https://images.guide/
+
+In production, use `new webpack.optimize.ModuleConcatenatePlugin()` to create fewer modules and reduce module overhead.
+Don't use this in development because it breaks hot module replacement.
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
     entry: ['./src/main.js', './scss/index.scss'],
@@ -17,14 +18,39 @@ module.exports = {
             {
                 test: /\.(sass|scss)$/,
                 loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    // inline files smaller than 10kb
+                    limit: 10 * 1024,
+                },
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-url-loader',
+                options: {
+                    // inline files smaller than 10kb
+                    limit: 10 * 1024,
+                    noquotes: true,
+                },
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)?/,
+                loader: 'image-webpack-loader',
+                enforce: 'pre',
             }
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"',
+        }),
         new ExtractTextPlugin({
             filename:'dist/[name].bundle.css',
             allChunks: true
         }),
-        new HtmlWebpackPlugin()
+        new webpack.optimize.UglifyJsPlugin(),
     ]
 };
